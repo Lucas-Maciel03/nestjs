57133878c742c3c22cb46ea75ae9c439ad388604
Lucas Maciel <farias.maciel@outlook.com>
2024-03-07 23:45:07 -0400

    criando entity Tag e importando nos modulos

--- primeiro-projeto/src/courses/courses.module.ts
+++ primeiro-projeto/src/courses/courses.module.ts
@@ -3,9 +3,10 @@ import { CoursesController } from './courses.controller';
 import { CoursesService } from './courses.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Course } from './entities/courses.entity';
+import { Tag } from './entities/tags.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Course])], //qual entidade esse modulo vai manipular?Course
+    imports: [TypeOrmModule.forFeature([Course, Tag])], //qual entidade esse modulo vai manipular?Course
     controllers: [CoursesController],
     providers: [CoursesService]
 })
