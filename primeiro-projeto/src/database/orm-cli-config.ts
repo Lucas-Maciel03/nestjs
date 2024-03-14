c37be9261132a9b17bc04c51e4ca0376a5b141c0
Lucas Maciel <farias.maciel@outlook.com>
2024-03-13 21:58:21 -0400

    Criando a tabela courses_tags através de migração

--- primeiro-projeto/src/database/orm-cli-config.ts
+++ primeiro-projeto/src/database/orm-cli-config.ts
@@ -2,11 +2,16 @@ import { dataSourceOptions } from "src/database/database.module";
 import { DataSource } from "typeorm";
 import { CreateCoursesTable1710214288467 } from "../migrations/1710214288467-CreateCoursesTable";
 import { CreateTagsTable1710378320012 } from "src/migrations/1710378320012-CreateTagsTable";
+import { CreateCoursesTagsTable1710381055785 } from "src/migrations/1710381055785-CreateCoursesTagsTable";
 
 export const dataSource = new DataSource({
     ... dataSourceOptions,
     synchronize: false,
-    migrations: [CreateCoursesTable1710214288467, CreateTagsTable1710378320012]
+    migrations: [
+        CreateCoursesTable1710214288467,
+        CreateTagsTable1710378320012,
+        CreateCoursesTagsTable1710381055785,
+    ]
     /*
         cada migração q for criada ira passar no arquivo de migrations,
         sera pego o que está no arquivo junto com as propriedades
